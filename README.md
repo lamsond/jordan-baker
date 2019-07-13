84a43134563f571545324b75049c28abf1e7178e
conrad <lamsond@wctech.org>
2019-07-13 11:05:59 -0400

    initial upload and typography plugin set-up

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # jordan-baker
+
 a gatsby tutorial using Typography.js plugin and nested layout components
+
+## plugins
+
+* using a theme called *fairy-gates* from `Typography.js` plugin
+* must add plugins to `gatsby-config` file
+* must add `src/utils/typography.js` config file
---
+++ tutorial-part-three/src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => (
+    <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+        <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+        <p>
+            What do I like to do? Make websites, play the guitar, read the bible, hang with my fam, etc.</p>
+    </div>
+);
