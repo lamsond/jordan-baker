2bfeec138f8396fb064a9e617324d1fd4d6d3596
conrad <lamsond@wctech.org>
2019-07-13 17:21:20 -0400

    added about and contact pages

---
+++ tutorial-part-three/src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => (
+    <div>
+        <h1>I'd love to talk! Email me at the address below</h1>
+        <p>
+            <a href='mailto:dlamson13@gmail.com'>dlamson13@gmail.com</a>
+        </p>
+    </div>
+);
+
